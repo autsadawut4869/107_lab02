3bbe2673d60fec51757b221dff739e9e855eaec4
autsadawut4869 <78350629+autsadawut4869@users.noreply.github.com>
2021-02-08 20:37:40 +0700

    edit

--- lab2_1.c
+++ lab2_1.c
@@ -5,34 +5,23 @@ int main()
     scanf("%d %d %d",&a,&b,&c);
     score=a+b+c;
 
-    if(score>=0&&score<=100)
-     {
-        if(score>=80)
-            printf("A");
-
-        else if(score>=75)
-            printf("B+");
-
-        else if(score>=70)
-            printf("B");
-
-        else if(score>=65)
-            printf("C+");
-
-        else if(score>=60)
-            printf("C");
-
-        else if(score>=55)
-            printf("D+");
-
-        else if(score>=50)
-            printf("D");
-
-        else
-            printf("F");
-    }
+    if(score>=80)
+        printf("A");
+    else if(score>=75)
+        printf("B+");
+    else if(score>=70)
+        printf("B");
+    else if(score>=65)
+        printf("C+");
+    else if(score>=60)
+        printf("C");
+    else if(score>=55)
+        printf("D+");
+    else if(score>=50)
+        printf("D");
     else
-    printf(" ");
+        printf("F");
+
 
     return 0;
 }
