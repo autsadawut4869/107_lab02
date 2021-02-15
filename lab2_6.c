62df34bbbda687fc3114ccdc1bd6aa13658d14c9
autsadawut4869 <78350629+autsadawut4869@users.noreply.github.com>
2021-02-15 15:49:56 +0700

    edit

--- lab2_6.c
+++ lab2_6.c
@@ -16,53 +16,53 @@ int main()
    for(i=0;i<=y;i++){
         if(a[i]==0){
             printf("Zero");
-            if(a[i+1]!='\0')
-                printf("-");
+            if(i!=y-1)
+            printf("-");
         }
         else if(a[i]==1){
             printf("One");
-            if(a[i+1]!= '\0')
-                printf("-");
+            if(i!=y-1)
+            printf("-");
         }
         else if(a[i]==2){
             printf("Two");
-            if(a[i+1]!='\0')
-                printf("-");
+            if(i!=y-1)
+            printf("-");
         }
         else if(a[i]==3){
             printf("Three");
-            if(a[i+1]!='\0')
-                printf("-");
+            if(i!=y-1)
+            printf("-");
         }
         else if(a[i]==4){
             printf("Four");
-            if(a[i+1]!= '\0')
-                printf("-");
+            if(i!=y-1)
+            printf("-");
         }
         else if(a[i]==5){
             printf("Five");
-            if(a[i+1]!= '\0')
-                printf("-");
+            if(i!=y-1)
+            printf("-");
         }
         else if(a[i]==6){
             printf("Six");
-            if(a[i+1]!= '\0')
-                printf("-");
+            if(i!=y-1)
+            printf("-");
         }
         else if(a[i]==7){
             printf("Seven");
-            if(a[i+1]!= '\0')
-                printf("-");
+            if(i!=y-1)
+            printf("-");
         }
         else if(a[i]==8){
             printf("Eight");
-            if(a[i+1]!= '\0')
-                printf("-");
+            if(i!=y-1)
+            printf("-");
         }
         else if(a[i]==9){
             printf("Nine");
-            if(a[i+1]!= '\0')
-                printf("-");
+            if(i!=y-1)
+            printf("-");
         }
     }
 
